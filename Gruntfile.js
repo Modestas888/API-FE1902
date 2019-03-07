6f37aa899d22014292a41b7676d7a22df400dae6
Modestas <modestaszk@gmail.com>
2019-03-07 21:19:48 +0200

    Smegenu lauzymas

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
               sassDir: 'src/assets/sass',
               imagesDir: 'src/img',
               cssDir: 'web/assets/css',
-              force: true,
+              force: true
           },
           dist: {
               options: {
@@ -32,7 +32,7 @@ module.exports = function(grunt) {
             layoutdir: 'src/templates/layouts',
             layout: ['default.hbs'],
             partials: ['src/templates/partials/{,*/}*.*', 'src/sprites/svg/*'],
-            helpers: ['partial'],
+            helpers: [''],
             flatten: true
         },
         en: {
@@ -42,6 +42,47 @@ module.exports = function(grunt) {
             src: ['src/templates/pages/en/*.hbs'],
             dest: './web'
         },
+    },
+    svg_sprite: {
+        generate: {
+            cwd: 'web/assets/vendor/material-design-icons',
+            src: [
+              '../../../../web/assets/images/ic_menu_24px.svg',
+              '../../../../web/assets/images/ic_menuius_24px.svg',
+              '../../../../web/assets/images/ic_ekranas_24px.svg',
+              '../../../../web/assets/images/ic_notifications_24px.svg',
+              '../../../../web/assets/images/ic_language_24px.svg',
+               '../../../../web/assets/images/ic_home_24px.svg',
+               '../../../../web/assets/images/ic_paint_24px.svg',
+               '../../../../web/assets/images/ic_settings_24px.svg',
+               '../../../../web/assets/images/ic_components_24px.svg',
+               '../../../../web/assets/images/ic_icons_24px.svg',
+               '../../../../web/assets/images/ic_forms_24px.svg',
+               '../../../../web/assets/images/ic_tables_24px.svg',
+               '../../../../web/assets/images/ic_charts_24px.svg',
+               '../../../../web/assets/images/ic_maps_24px.svg',
+               '../../../../web/assets/images/ic_mail_24px.svg',
+               '../../../../web/assets/images/ic_pages_24px.svg',
+               '../../../../web/assets/images/ic_extras_24px.svg',
+               '../../../../web/assets/images/ic_multilevel_24px.svg',
+            ],
+            dest: 'src/sprites',
+            options: {
+                shape: {
+                    id: {
+                        generator: function(filename) {
+                            var id = filename.match(/ic_(\w+)_\d+/);
+                            return id[1];
+                        }
+                    },
+                },
+                mode: {
+                    symbol: {
+                        dest: ''
+                    }
+                }
+            }
+        }
     },
     watch: {
         options: {
@@ -55,45 +96,6 @@ module.exports = function(grunt) {
             tasks: ['assemble:en']
         }
     },
-    svg_sprite: {
-       generate: {
-           cwd: 'web/assets/vendor/material-design-icons',
-           src: [
-               '../../../../web/assets/images/ic_menu_24px.svg',
-               '../../../../web/assets/images/ic_notifications_24px.svg',
-               '../../../../web/assets/images/ic_language_24px.svg',
-                '../../../../web/assets/images/ic_home_24px.svg',
-                '../../../../web/assets/images/ic_paint_24px.svg',
-                '../../../../web/assets/images/ic_settings_24px.svg',
-                '../../../../web/assets/images/ic_components_24px.svg',
-                '../../../../web/assets/images/ic_icons_24px.svg',
-                '../../../../web/assets/images/ic_forms_24px.svg',
-                '../../../../web/assets/images/ic_tables_24px.svg',
-                '../../../../web/assets/images/ic_charts_24px.svg',
-                '../../../../web/assets/images/ic_maps_24px.svg',
-                '../../../../web/assets/images/ic_mail_24px.svg',
-                '../../../../web/assets/images/ic_pages_24px.svg',
-                '../../../../web/assets/images/ic_extras_24px.svg',
-                '../../../../web/assets/images/ic_multilevel_24px.svg',
-           ],
-           dest: 'src/sprites',
-           options: {
-               shape: {
-                   id: {
-                       generator: function(filename) {
-                           var id = filename.match(/ic_(\w+)_\d+/);
-                           return id[1];
-                       }
-                   },
-               },
-               mode: {
-                   symbol: {
-                       dest: ''
-                   }
-               }
-           }
-       }
-   },
   });
 
   [
