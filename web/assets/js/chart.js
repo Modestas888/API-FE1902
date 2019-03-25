0ad0d8a2c0172ab4487e1e3afe3adb9abeb7f87c
Modestas <modestaszk@gmail.com>
2019-03-25 19:22:51 +0200

    chartas veikia, button tvarkymas

--- web/assets/js/chart.js
+++ web/assets/js/chart.js
@@ -2,7 +2,6 @@ var ctx = document.getElementById('myChart').getContext('2d');
 var chart = new Chart(ctx, {
     // The type of chart we want to create
     type: 'line',
-
     // The data for our dataset
     data: {
         labels: ["January", "February", "March", "April", "May", "June", "July"],
