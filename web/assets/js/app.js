4594e8985bf74b2e84c8985a6512dbe35f9d1e9a
Modestas <modestaszk@gmail.com>
2019-03-27 21:08:51 +0200

    sidenav right, login, sidenav taisymai

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -3,6 +3,10 @@ $(function() {
     $('#side-menu').toggleClass('side-menu--closed');
     $('body').toggleClass('sidenav-closed');
   })
+  $(`#notification`).on(`click`, function () {
+    $(`#side-menu-right`).toggleClass(`side-menu--right--closed`)
+
+  })
 var basename = $('#side-menu').find('li.active span').text();
 $('#basename')
 .text (basename)
